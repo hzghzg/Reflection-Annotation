a7dcadfc736bf63412e2a895abc092059b9beaf7
hzghzg <2452376183@qq.com>
2020-08-23 07:48:26 +0800

    init springboot

---
+++ src/main/java/com/huangda7/reflectionandannotation/ReflectionandannotationApplication.java
@@ -0,0 +1,13 @@
+package com.huangda7.reflectionandannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectionandannotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReflectionandannotationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huangda7/reflectionandannotation/ReflectionandannotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangda7.reflectionandannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectionandannotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
